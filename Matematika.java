0ed67c845b652cd9fc11483a255c10e95ff2173c
Ggskrn <148623788+Ggskrn@users.noreply.github.com>
2024-09-18 08:16:05 +0700

    Matematika.java

---
+++ Matematika.java
@@ -0,0 +1,19 @@
+jetbrains://idea/navigate/reference?project=TUGAS 4&fqn=Matematika_Gagas3f.Matematika
+C:\Users\USER\IdeaProjects\TUGAS 4\src\Matematika_Gagas3f\Matematika.java
+
+  package Matematika_Gagas3f;
+
+public class Matematika {
+    public int pertambahan(int a, int b) {
+        return a + b;
+    }
+    public int pengurangan(int a, int b) {
+        return a - b;
+    }
+    public int perkalian(int a, int b) {
+        return a * b;
+    }
+    public int pembagian(int a, int b) {
+        return a / b;
+    }
+}
