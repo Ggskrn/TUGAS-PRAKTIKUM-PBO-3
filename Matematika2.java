6f0591660bf2ae46320772ee1669526e4dd342a8
Ggskrn <148623788+Ggskrn@users.noreply.github.com>
2024-09-18 08:17:33 +0700

    Matematika2.java

---
+++ Matematika2.java
@@ -0,0 +1,10 @@
+jetbrains://idea/navigate/reference?project=TUGAS 4&fqn=Matematika_Gagas3f.Matematika2
+C:\Users\USER\IdeaProjects\TUGAS 4\src\Matematika_Gagas3f\Matematika2.java
+
+  package Matematika_Gagas3f;
+
+public class Matematika2 extends Matematika {
+    public int modulus (int a, int b) {
+        return a % b;
+    }
+}
